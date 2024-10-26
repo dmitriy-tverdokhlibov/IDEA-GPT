cad73c8f81399ae1a6fab341d3ec4ca96f501588
dmitriy-tverdokhlibov <dmitriytverdokhlibov.yahoo.com>
2024-10-26 19:39:31 +0200

    Initial commit

---
+++ src/main/java/org/example/plugin/IdeaGptApp.java
@@ -0,0 +1,14 @@
+package org.example.plugin;
+
+
+/**
+ * The IdeaGptApp class is responsible for managing the core functionalities
+ * of the Idea Generation Platform powered by GPT.
+ * This class serves as the main entry point for the application.
+ *
+ * @author dmitriy-tverdokhlibov
+ */
+
+public class IdeaGptApp {
+
+}
